ddd791e5c8cd3fc15eb4f00372f780f5544badfd
Matteo Cappella <matteo.cappella@leitha.eu>
2022-03-28 13:16:35 +0200

    upload fastapi test

---
+++ README.md
@@ -0,0 +1,2 @@
+Per eseguire l'applicazione: 
+uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = Optional[None]
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
